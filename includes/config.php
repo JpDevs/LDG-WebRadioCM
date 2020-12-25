f65c5646c23d3712509385fdc47a52b00a8e241d
JpDevs <contato@jpdevs.host>
2020-12-25 20:21:06 -0300

    Resolução do bug no charset utf8 na db mysql

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 //Desenvolvido por JpDevs
 require_once "auth.php";  //
 $mysqli = new mysqli($host, $usuario, $senha, $bd);
-
+$mysqli -> set_charset("utf8");
 if($mysqli->connect_errno) {
     echo "Falha na conexão com a data-base, verifique se a instalação foi feita corretamente.";
     header("Location:install");
